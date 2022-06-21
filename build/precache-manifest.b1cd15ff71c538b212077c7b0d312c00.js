46fe40604f5b314b266b9d09faf3c6193299699a
Matheus Moura <61365431+Mouramattheus@users.noreply.github.com>
2022-06-20 23:11:50 -0300

    Add files via upload

---
+++ build/precache-manifest.b1cd15ff71c538b212077c7b0d312c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22caf55184e1bd3bf077",
+    "url": "/static/css/main.4e251dca.chunk.css"
+  },
+  {
+    "revision": "22caf55184e1bd3bf077",
+    "url": "/static/js/main.22caf551.chunk.js"
+  },
+  {
+    "revision": "9714c91fcadfe2f2bce1",
+    "url": "/static/js/1.9714c91f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6bda7b9640dbdec795036bb08230584d",
+    "url": "/index.html"
+  }
+];
